b86132b25bfe2b41e23a5a7b12c6b26baf86b9fb
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2021-02-05 23:22:42 +0000

    create priciples model, controller, migration and seeder files

---
+++ app/Http/Controllers/PrinciplesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Principles;
+use Illuminate\Http\Request;
+
+class PrinciplesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Principles  $principles
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Principles $principles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Principles  $principles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Principles $principles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Principles  $principles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Principles $principles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Principles  $principles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Principles $principles)
+    {
+        //
+    }
+}
---
+++ database/factories/PrinciplesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Principles;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PrinciplesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Principles::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
